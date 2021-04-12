99f426c7f31255aec462fa29a7e15c6c5891c912
kanaasw <aswink30@gmail.com>
2021-04-11 22:41:12 -0400

    Add APIs for the messages

---
+++ controller/message_controller.go
@@ -0,0 +1,125 @@
+package controller
+
+import (
+	"encoding/json"
+	"github.com/ash822/goweb/entity"
+	"github.com/ash822/goweb/service"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type MessageController interface {
+	GetMessageById(resw http.ResponseWriter, req *http.Request)
+	GetAllMessages(resw http.ResponseWriter, req *http.Request)
+	CreateMessage(resw http.ResponseWriter, req *http.Request)
+	UpdateMessage(resw http.ResponseWriter, req *http.Request)
+	DeleteMessage(resw http.ResponseWriter, req *http.Request)
+}
+
+var svc service.MessageService
+
+type controller struct{}
+
+func GetInstance(msgSvc service.MessageService) MessageController {
+	svc = msgSvc
+	return &controller{}
+}
+
+func (*controller) GetMessageById(resw http.ResponseWriter, req *http.Request) {
+	resw.Header().Set("Content-type", "application/json")
+
+	params := mux.Vars(req)
+	id := params["id"]
+
+	msg, err := svc.FindById(id)
+
+	if err != nil {
+		resw.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(resw).Encode([]byte(`{"error": "` + "err.Error()" + `"}`))
+	}
+
+	resw.WriteHeader(http.StatusOK)
+	json.NewEncoder(resw).Encode(msg)
+}
+
+func (*controller) GetAllMessages(resw http.ResponseWriter, _ *http.Request) {
+	resw.Header().Set("Content-type", "application/json")
+
+	msgs, err := svc.FindAll()
+
+	if err != nil {
+		resw.WriteHeader(http.StatusInternalServerError)
+		resw.Write([]byte(`{"error": "Encountered error fetching the messages"`))
+	} else {
+		resw.WriteHeader(http.StatusOK)
+		json.NewEncoder(resw).Encode(msgs)
+	}
+}
+
+func (*controller) CreateMessage(resw http.ResponseWriter, req *http.Request) {
+	resw.Header().Set("Content-type", "application/json")
+
+	var msg entity.Message
+	var err error
+
+	json.NewDecoder(req.Body).Decode(&msg)
+
+	msg.Id = uuid.New().String()
+	msg.Palindrome, err = svc.IsPalindrome(msg.Text)
+
+	if err != nil {
+		resw.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(resw).Encode([]byte(`{"error": "` + err.Error() + `"}`))
+	} else {
+		result, err := svc.Create(&msg)
+
+		if err != nil {
+			resw.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(resw).Encode([]byte(`{"error": "` + err.Error() + `"}`))
+			return
+		}
+
+		resw.WriteHeader(http.StatusCreated)
+		json.NewEncoder(resw).Encode(result)
+	}
+}
+
+func (*controller) UpdateMessage(resw http.ResponseWriter, req *http.Request) {
+	resw.Header().Set("Content-type", "application/json")
+
+	var msg entity.Message
+
+	params := mux.Vars(req)
+	id := params["id"]
+
+	json.NewDecoder(req.Body).Decode(&msg)
+
+	result, err := svc.Update(id, &msg)
+
+	if err != nil {
+		resw.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(resw).Encode([]byte(`{"error": "` + err.Error() + `"}`))
+		return
+	}
+
+	resw.WriteHeader(http.StatusOK)
+	json.NewEncoder(resw).Encode(result)
+}
+
+func (*controller) DeleteMessage(resw http.ResponseWriter, req *http.Request) {
+	resw.Header().Set("Content-type", "application/json")
+
+	params := mux.Vars(req)
+	id := params["id"]
+
+	err := svc.Delete(id)
+
+	if err != nil {
+		resw.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(resw).Encode([]byte(`{"error": "` + err.Error() + `"}`))
+		return
+	}
+
+	resw.WriteHeader(http.StatusNoContent)
+}
---
+++ repository/message_repository.go
@@ -0,0 +1,104 @@
+package repository
+
+import (
+	"errors"
+	"github.com/ash822/goweb/entity"
+)
+
+type MessageRepository interface {
+	FindById(id string) (*entity.Message, error)
+	FindAll() ([]entity.Message, error)
+	Create(msg *entity.Message) (*entity.Message, error)
+	Update(newMsg *entity.Message) (*entity.Message, error)
+	Delete(id string) error
+}
+
+var (
+	msgs []entity.Message
+)
+
+type repo struct{}
+
+func GetInstance() MessageRepository {
+	return &repo{}
+}
+
+func (*repo) Create(msg *entity.Message) (*entity.Message, error) {
+	msgs = append(msgs, entity.Message{
+		Id: msg.Id,
+		Text: msg.Text,
+		Palindrome: msg.Palindrome,
+	})
+
+	return msg, nil
+}
+
+func (*repo) Update(newMsg *entity.Message) (*entity.Message, error) {
+	if newMsg.Id == "" {
+		return nil, errors.New("the id provided is invalid")
+	}
+
+	var index = -1
+	for i, msg := range msgs {
+		if newMsg.Id == msg.Id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		return nil, errors.New("a message does not exists for the given Id:" + newMsg.Id)
+	} else {
+		msgs[index] = entity.Message{
+			Id: newMsg.Id,
+			Text: newMsg.Text,
+			Palindrome: newMsg.Palindrome,
+		}
+
+		return newMsg, nil
+	}
+}
+
+func (*repo) Delete(id string) error {
+	if id == "" {
+		return errors.New("the id provided is invalid")
+	}
+
+	var index = -1
+	for i, msg := range msgs {
+		if id == msg.Id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		return errors.New("a message does not exists for the given Id:" + id)
+	} else {
+		msgs[index] = msgs[len(msgs)-1]
+		msgs[len(msgs)-1] = entity.Message{}
+		msgs = msgs[:len(msgs)-1]
+
+		return nil
+	}
+}
+
+func (*repo) FindById(id string) (*entity.Message, error) {
+	if id == "" {
+		return nil, errors.New("the id provided is invalid")
+	}
+
+	for _, msg := range msgs {
+		if id == msg.Id {
+			return &msg, nil
+		}
+	}
+
+	return nil, errors.New("a message does not exists for the given Id:" + id)
+}
+
+func (*repo) FindAll() ([]entity.Message, error) {
+	if len(msgs) == 0 {
+		msgs = []entity.Message{}
+	}
+	return msgs, nil
+}
